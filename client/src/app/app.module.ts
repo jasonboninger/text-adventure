381bc06c7df5561eb9be8c6189480c0f1be9e45b
John Coppola <jcoppola@insiten.com>
2020-04-12 18:14:45 -0400

    creates home, shared, and app-routing modules; creates shell, home, and page-not-found components

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,12 +1,16 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ShellComponent } from "./components/shell/shell.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
 
 @NgModule({
 	declarations: [
-		AppComponent
+		AppComponent,
+		ShellComponent,
+		PageNotFoundComponent
 	],
 	imports: [
 		BrowserModule,
