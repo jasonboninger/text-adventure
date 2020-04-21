70aa973ef5e411fd54fd5929436259cef08e957f
Jason Boninger <jason.boninger@gmail.com>
2020-04-21 19:06:14 -0400

    Removed change.

--- designer/types/change.ts
+++
@@ -1,8 +0,0 @@
-import { IDictionary } from "./core";
-
-export interface IChange {
-	item?: { id: string } & IDictionary<string>;
-	area?: { id: string } & IDictionary<string>;
-	player?: IDictionary<string>;
-	game?: IDictionary<string>;
-}
