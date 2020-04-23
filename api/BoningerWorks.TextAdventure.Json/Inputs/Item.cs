cd32d7986584c9b613169c198391d2577d1bd72f
Jason Boninger <jason.boninger@gmail.com>
2020-04-22 20:27:45 -0400

    Fixed item.

--- api/BoningerWorks.TextAdventure.Json/Inputs/Item.cs
+++ api/BoningerWorks.TextAdventure.Json/Inputs/Item.cs
@@ -1,4 +1,5 @@
 ﻿using BoningerWorks.TextAdventure.Json.Utilities;
+using System.Collections.Generic;
 using System.Text.Json.Serialization;
 
 namespace BoningerWorks.TextAdventure.Json.Inputs
@@ -8,5 +9,6 @@ namespace BoningerWorks.TextAdventure.Json.Inputs
 		[JsonPropertyName("names")] public OneOrManyList<string> Names { get; set; }
 		[JsonPropertyName("active")] public bool? Active { get; set; }
 		[JsonPropertyName("reactions")] public OneOrManyList<Reaction> Reactions { get; set; }
+		[JsonPropertyName("items")] public Dictionary<string, Item> ItemSymbolToItemMappings { get; set; }
 	}
 }
--- designer/types/item.ts
+++ designer/types/item.ts
@@ -1,9 +1,9 @@
-import { IOneOrArray } from "./core";
+import { IOneOrArray, IDictionary } from "./core";
 import { IReaction } from "./reaction";
 
 export interface IItem {
 	names: IOneOrArray<string>;
 	active?: boolean;
 	reactions?: IOneOrArray<IReaction>;
-	items?: IOneOrArray<IItem>;
+	items?: IDictionary<IItem>;
 }
