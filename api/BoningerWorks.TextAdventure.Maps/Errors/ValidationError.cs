8e883682efadffdab52c7cf8a483073454e6629d
Jason Boninger <jason.boninger@gmail.com>
2020-04-23 22:19:02 -0400

    Added validation error and global suppressions.

---
+++ api/BoningerWorks.TextAdventure.Maps/Errors/ValidationError.cs
@@ -0,0 +1,48 @@
+﻿using BoningerWorks.TextAdventure.Core.Exceptions;
+using BoningerWorks.TextAdventure.Core.Interfaces;
+using System;
+
+namespace BoningerWorks.TextAdventure.Maps.Errors
+{
+	public class ValidationError : IError
+	{
+		public static implicit operator GenericException<ValidationError>(ValidationError? validationError)
+		{
+			// Return generic exception
+			return GenericException.Create(validationError);
+		}
+
+		public string Message { get; }
+
+		public ValidationError(string message)
+		{
+			// Set message
+			Message = message;
+		}
+
+		public GenericException<ValidationError> ToGenericException(GenericException<ValidationError>? innerException = null)
+		{
+			// Return generic exception
+			return ToGenericException((Exception?)innerException);
+		}
+		public GenericException<ValidationError> ToGenericException(Exception? innerException = null)
+		{
+			// Check if inner exception is validation error generic exception
+			if (innerException is GenericException<ValidationError> innerGenericException)
+			{
+				// Create message
+				var message = Message;
+				// Check if error exists and message exists
+				if (innerGenericException.Error != null && !string.IsNullOrWhiteSpace(innerGenericException.Message))
+				{
+					// Add message
+					message += " " + innerGenericException.Error.Message;
+				}
+				// Return generic exception
+				return GenericException.Create(new ValidationError(message), innerGenericException);
+			}
+			// Return generic exception
+			return GenericException.Create(this, innerException);
+		}
+	}
+}
---
+++ api/BoningerWorks.TextAdventure.Maps/GlobalSuppressions.cs
@@ -0,0 +1,8 @@
+﻿// This file is used by Code Analysis to maintain SuppressMessage
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given
+// a specific target and scoped to a namespace, type, member, etc.
+
+using System.Diagnostics.CodeAnalysis;
+
+[assembly: SuppressMessage("Minor Code Smell", "S3626:Jump statements should not be redundant", Justification = "Not always right in lambda functions.")]
