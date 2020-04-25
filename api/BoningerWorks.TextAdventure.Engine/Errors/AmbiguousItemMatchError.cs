c0061b19d4338693c94d281a1320a1084e5369be
Jason Boninger <jason.boninger@gmail.com>
2020-04-25 10:58:48 -0400

    Fixed error classes.

---
+++ api/BoningerWorks.TextAdventure.Engine/Errors/AmbiguousItemMatchError.cs
@@ -0,0 +1,23 @@
+﻿using BoningerWorks.TextAdventure.Core.Interfaces;
+using BoningerWorks.TextAdventure.Core.Utilities;
+using BoningerWorks.TextAdventure.Engine.Executables;
+using System.Collections.Immutable;
+
+namespace BoningerWorks.TextAdventure.Engine.Errors
+{
+	public class AmbiguousItemMatchError : IError
+	{
+		public string Message => $"Name ({Name}) matches more than one item.";
+
+		public Name Name { get; }
+		public ImmutableArray<Item> Items { get; }
+
+		public AmbiguousItemMatchError(Name name, ImmutableArray<Item> items)
+		{
+			// Set name
+			Name = name;
+			// Set items
+			Items = items;
+		}
+	}
+}
