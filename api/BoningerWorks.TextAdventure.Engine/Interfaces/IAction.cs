f0a8f8176cc8b12f5f568c84792af239128b4ebb
Jason Boninger <jason.boninger@gmail.com>
2020-04-26 17:34:51 -0400

    Added action interface.

---
+++ api/BoningerWorks.TextAdventure.Engine/Interfaces/IAction.cs
@@ -0,0 +1,11 @@
+﻿using BoningerWorks.TextAdventure.Json.States;
+using System.Collections.Generic;
+
+namespace BoningerWorks.TextAdventure.Engine.Interfaces
+{
+	public interface IAction<out TValue>
+	where TValue : class
+	{
+		IEnumerable<TValue> Execute(GameState gameState);
+	}
+}
