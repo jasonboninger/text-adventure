c1dee5574e57fb9a21a16c60409149eff2927d3f
Jason Boninger <jason.boninger@gmail.com>
2020-05-09 09:45:44 -0400

    Removed dictionary.

--- designer/types/core.ts
+++ designer/types/core.ts
@@ -1,7 +1,5 @@
 import { XCondition } from "./condition";
 
-export type XDictionary<TValue> = { [id: string]: TValue };
-
 export type XOneOrArray<TValue> = TValue | TValue[];
 
 export interface IIf<TValue> {
