c67de6b4329b6a573c8e81827926c6644886261b
Jason Boninger <jason.boninger@gmail.com>
2020-05-11 18:53:58 -0700

    Added metadata static class and added location metadata to item.

---
+++ api/BoningerWorks.TextAdventure.Engine/Static/Metadata.cs
@@ -0,0 +1,11 @@
+﻿using BoningerWorks.TextAdventure.Core.Utilities;
+
+namespace BoningerWorks.TextAdventure.Engine.Static
+{
+	public static class Metadata
+	{
+		public static Id Id { get; } = new Id("ID");
+		public static Id Name { get; } = new Id("NAME");
+		public static Id Location { get; } = new Id("LOCATION");
+	}
+}
--- api/BoningerWorks.TextAdventure.Engine/Structural/Area.cs
+++ api/BoningerWorks.TextAdventure.Engine/Structural/Area.cs
@@ -8,9 +8,6 @@ namespace BoningerWorks.TextAdventure.Engine.Structural
 {
 	public class Area : IEntity
 	{
-		private static readonly Id _datumId = new Id("ID");
-		private static readonly Id _datumName = new Id("NAME");
-
 		public Id Id { get; }
 		public Names Names { get; }
 		public ImmutableDictionary<Id, string> Metadata { get; }
@@ -24,8 +21,8 @@ namespace BoningerWorks.TextAdventure.Engine.Structural
 			// Create metadata
 			Metadata = ImmutableDictionary.CreateRange(new KeyValuePair<Id, string>[]
 			{
-				KeyValuePair.Create(_datumId, Id.ToString()),
-				KeyValuePair.Create(_datumName, Names.Name.ToString())
+				KeyValuePair.Create(Static.Metadata.Id, Id.ToString()),
+				KeyValuePair.Create(Static.Metadata.Name, Names.Name.ToString())
 			});
 		}
 
